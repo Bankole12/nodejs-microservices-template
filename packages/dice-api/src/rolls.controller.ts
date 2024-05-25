be7bb01a85b8ebe6fc79a8ab1175d86a1aa304f7
Bankole Simon <simonbankole99@gmail.com>
2024-05-25 02:20:57 +0000

    database updated for setting-api and dice-api

--- packages/dice-api/src/rolls.controller.ts
+++ packages/dice-api/src/rolls.controller.ts
@@ -31,12 +31,10 @@ export class RollsController {
   @Get('history')
   async getRollsHistory(
     @Query('max', new DefaultValuePipe(10), ParseIntPipe) max: number,
-    @Query('sides', new DefaultValuePipe(6), ParseIntPipe) sides: number
+    @Query('sides', new DefaultValuePipe(6), ParseIntPipe) sides: number,
   ) {
     this.logger.log(`Retrieving last ${max} rolls history [sides: ${sides}]`);
     const rolls = await this.db.getLastRolls(max, sides);
     return { result: rolls.map((roll) => roll.result) };
   }
-
-
 }
